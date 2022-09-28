040c27e367ed15fc3b44804130d6e93fbd269c8c
Md-TarikulIslam <nayon1820@gmail.com>
2022-09-28 23:03:24 +0600

    button added

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -7,11 +7,17 @@ const Activity = (props) => {
     return (
         <div className='activity'>
             <img src={picture} alt="" />
-            <h2>{name}</h2>
-            {/* <p>{about}</p> */}
-            <p>For Age: {age}</p>
-            <p>Time required: {time}s</p>
+            <div className='activity-info'>
+                <h2>{name}</h2>
+                <p>{about}</p>
+                <p>For Age: {age}</p>
+                <p>Time required: {time}s</p>
+            </div>
+            <button className='btn-list'>
+                <p>Add to list</p>
+            </button>
         </div>
+
     );
 };
 

5c87893ebd6d0904b686571017fdb4b5bbaf93f5
Md-TarikulIslam <nayon1820@gmail.com>
2022-09-29 14:30:13 +0600

    styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Activities from './components/Activities/Activities';
 import Header from './components/Header/Header';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Activities></Activities>
+      <ToastContainer />
     </div>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './Cart.css'
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Cart = ({ cart }) => {
 
+    const [Break, setBreak] = useState(0);
+
+    const notify = () => toast.success("Congratulations! Mission Completed");
+
+    const breakHandle = (temp) => {
+        setBreak(temp);
+    }
     let total = 0;
     for (const activity of cart) {
         total = total + activity.time;
@@ -10,14 +19,35 @@ const Cart = ({ cart }) => {
     return (
 
         <div className='cart'>
+            <h2 className='name'>Md. Tarikul Islam</h2>
+            <h5>Natore, Bangladesh</h5>
+            <div className='personal-status'>
+                <div>
+                    <h3>70KG</h3>
+                    <h5>Weight</h5>
+                </div>
+                <div>
+                    <h3>5.6</h3>
+                    <h5>Height</h5>
+                </div>
+                <div>
+                    <h3>22</h3>
+                    <h5>Age</h5>
+                </div>
+            </div>
 
             <h2>Add a break</h2>
-            <button>20</button>
+            <div className="br-div">
+                <button className='br-btn' onClick={() => breakHandle(30)}>30</button>
+                <button className='br-btn' onClick={() => breakHandle(40)}>40</button>
+                <button className='br-btn' onClick={() => breakHandle(50)}>50</button>
+                <button className='br-btn' onClick={() => breakHandle(20)}>20</button>
+            </div>
             <h2 className='details'>Exercise details</h2>
 
             <p className='time'>Exercise Time: {total}</p>
-            <p className='break'>Break Time: </p>
-            <button className='btn-activity'><p>Activity Completed</p></button>
+            <p className='break'>Break Time: {Break}</p>
+            <button onClick={notify} className="btn-activity"><p>Activity Completed</p></button>
         </div>
     );
 };
